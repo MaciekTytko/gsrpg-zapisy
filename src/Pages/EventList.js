2030e4c9a5c281adc9152c9fef4496486d9a0ada
MaciekTytko <106463945+MaciekTytko@users.noreply.github.com>
2022-12-03 16:21:48 +0100

    useAccordion

--- src/Pages/EventList.js
+++ src/Pages/EventList.js
@@ -5,54 +5,80 @@ import EventCard from "../Components/EventCard";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useState } from "react";
 
 function EventList() {
+  const [expanded, setExpanded] = useState(0);
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : -1);
+  };
+
   return (
     <div>
-
-
-
-
-
-
-      
-      <Typography variant="h2" sx={{textAlign: 'left'}}>Grudzień</Typography>
-      <Box sx={{
-        bgcolor: '#cfe8fc',
-        padding: '20px 20px',
-        display: 'flex',
-        flexDirection: 'row', //TODO row-reverse
-        flexWrap: 'wrap', //TODO wrap-reverse
-        justifyContent: 'space-evenly',
-
-      }} >
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-
-      </Box>
-      <Typography variant="h2" sx={{textAlign: 'left'}}>Listopad</Typography>
-      <Box sx={{
-        bgcolor: '#cfe8fc',
-        padding: '20px 20px',
-        display: 'flex',
-        flexDirection: 'row', //TODO row-reverse
-        flexWrap: 'wrap', //TODO wrap-reverse
-        justifyContent: 'space-evenly',
-
-      }} >
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-
-      </Box>
+      <Accordion expanded={expanded == 0} onChange={handleChange(0)}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography variant="h4">Grudzień</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <EventBox />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded == 1} onChange={handleChange(1)}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography variant="h4">Listopad</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <EventBox />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded == 2} onChange={handleChange(2)}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography variant="h4">Październik</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <EventBox />
+        </AccordionDetails>
+      </Accordion>
     </div>
   )
 }
+
+function EventBox() {
+  return (
+    <>
+      <Box sx={{
+        bgcolor: '#cfe8fc',
+        padding: '20px 20px',
+        display: 'flex',
+        flexDirection: 'row', //TODO row-reverse
+        flexWrap: 'wrap', //TODO wrap-reverse
+        justifyContent: 'space-evenly',
+
+      }} >
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+
+      </Box>
+    </>
+  )
+}
+
+
 export default EventList;
