bb972b965d09fb50965f55dba5c1a38f2131aa4e
MaciekTytko <106463945+MaciekTytko@users.noreply.github.com>
2022-12-04 10:13:04 +0100

    Menu template

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import Main from "./Containers/Main"
 import Footer from "./Containers/Footer"
 import store from './Redux/Store'
 import { Provider } from 'react-redux'
-import { Counter } from './Redux/Counter';
 import MenuSite from "./Containers/MenuSite"
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Header />
         <MenuSite/>
+        <Header />
         <Main />
         <Footer />
       </div>
