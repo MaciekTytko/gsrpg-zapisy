2202ea7ecf220737d0e565333bc8c1b5a5512622
MaciekTytko <106463945+MaciekTytko@users.noreply.github.com>
2022-12-04 21:20:25 +0100

    Change Box to Grid

--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -1,5 +1,4 @@
-import { Box, Paper, Typography, Button, Container } from "@mui/material";
-import { useEffect } from "react";
+import { Box, Paper, Typography, Button, Container, Grid } from "@mui/material";
 import { useNavigate } from "react-router";
 import { useAuthUserData } from "../Hooks/useAuth";
 
@@ -7,40 +6,51 @@ import { useAuthUserData } from "../Hooks/useAuth";
 
 function UserProfile() {
   const user = useAuthUserData();
-  const navigator = useNavigate();
 
   return (
-    <Container sx={{display: 'flex',justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+    <Container sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
       <Typography
-          sx={{m: 2}}
-          variant="h4">
-          Profil użytkownika
-        </Typography>
-      <Paper sx={{width: 600, p:2, m: 1}}>
-        <Box sx={{mb: 2, display: 'flex',flexDirection: 'row',justifyContent: 'center' }}>
-          <Paper sx={{mr: 2, pl: 2, pr: 2, display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center',
-        minWidth: 200  }}>
-            <Typography>{user.email}</Typography>
-          </Paper>
-          <Button variant="contained">
-            zmień email
-          </Button>
-        </Box>
-        <Box sx={{mb: 2, display: 'flex',flexDirection: 'row',justifyContent: 'center' }}>
-          <Paper sx={{mr: 2, pl: 2, pr: 2, display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center',
-        minWidth: 200  }}>
-            <Typography># # # # # # # #</Typography>
-          </Paper>
-          <Button variant="contained">
-            zmień hasło
-          </Button>
-        </Box>
-        <Box sx={{display: 'flex', justifyContent: 'right' }}>
-        <Button 
-        sx={{m: 1}} 
-        variant="outlined"
-        onClick={()=>navigator('events')}
-        >Przejdź do listy wydarzeń</Button>
+        sx={{ m: 2 }}
+        variant="h4">
+        Profil użytkownika
+      </Typography>
+      <Paper sx={{ width: 600, p: 2, m: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        <Grid sx={{ mb: 2, }} container spacing={2}>
+          <Grid item xs={8}>
+            <Paper sx={{
+              mr: 2, pl: 2, pr: 2, height: '100%', minWidth: 200,
+              display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center',
+            }}>
+              <Typography>{user.email}</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="contained">
+              zmień email
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid sx={{ mb: 2, }} container spacing={2}>
+          <Grid item xs={8}>
+            <Paper sx={{
+              mr: 2, pl: 2, pr: 2, height: '100%', minWidth: 200,
+              display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center',
+            }}>
+              <Typography># # # # # # # #</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="contained">
+              zmień hasło
+            </Button>
+          </Grid>
+        </Grid>
+        <Box sx={{ display: 'flex', justifyContent: 'right' }}>
+          <Button
+            sx={{ m: 1 }}
+            variant="outlined"
+            onClick={() => navigator('events')}
+          >Przejdź do listy wydarzeń</Button>
         </Box>
       </Paper>
     </Container>
