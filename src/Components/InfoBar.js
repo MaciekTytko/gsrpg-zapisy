585ea562c57be27ce8fd31da9b1b0adfc534774e
MaciekTytko <106463945+MaciekTytko@users.noreply.github.com>
2023-01-21 00:33:38 +0100

    modify InfoBar

--- src/Components/InfoBar.js
+++ src/Components/InfoBar.js
@@ -1,15 +1,27 @@
-import { Alert, Snackbar } from "@mui/material";
+import { Alert, Slide, Snackbar } from "@mui/material";
 import { useContext } from "react";
 import InfoBarContext from "../Context/InfoBarContext";
 import { infoBarAction } from "../Reduce/InfoBarReducer";
 
+function Transition(props) {
+  return <Slide {...props} direction="right" />;
+}
 
 function InfoBar(){
   const infobar = useContext(InfoBarContext);
 
   return (
-    <Snackbar open={infobar.state.open} autoHideDuration={2500} onClose={() => infobar.dispatch({type: infoBarAction.CLOSE})}>
-      <Alert onClose={() => infobar.dispatch({type: infoBarAction.CLOSE})} severity={infobar.state.severity} variant="filled" sx={{ width: '100%' }}>
+    <Snackbar 
+    open={infobar.state.open} 
+    autoHideDuration={2500} 
+    onClose={() => infobar.dispatch({type: infoBarAction.CLOSE})}
+    TransitionComponent={Transition}
+    >
+      <Alert 
+      onClose={() => infobar.dispatch({type: infoBarAction.CLOSE})} 
+      severity={infobar.state.severity} 
+      variant="filled" 
+      sx={{ width: '100%' }}>
         {infobar.state.message}
       </Alert>
     </Snackbar>
