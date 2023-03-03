4d8594743041fdc86d6827d80d7ce991384cb6ae
MaciekTytko <106463945+MaciekTytko@users.noreply.github.com>
2023-03-03 20:50:21 +0100

    Fix footer

--- src/Containers/Footer.js
+++ src/Containers/Footer.js
@@ -1,19 +1,31 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, IconButton } from "@mui/material";
+import ExternalLink from "@mui/material/Link"
+import { Link } from "react-router-dom";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import discordIcon from "../Assets/Logo/discord-mark-black.svg"
 import "./Footer.scss"
 
 function Footer() {
   return (
     <div className="Foot d-print-none">
       <Container>
-        <Box sx={{ pt:'30px', display: 'flex', justifyContent: 'space-around' }}>
+        <Box sx={{ pt: '30px', display: 'flex', justifyContent: 'space-around' }}>
           <Box>
             <p>O serwisie</p>
             <p>Polityka prywatności</p>
+            <p>Materiały promocyjne</p>
           </Box>
-          <Box>
-            <p>Liga ZMG</p>
-            <p>Kontakt</p>
-            <p>sociale</p>
+          <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }}>
+            <ExternalLink href="http://www.ligazmg.pl">Liga ZMG</ExternalLink>
+            <Link to="/contact">Kontakt</Link>
+            <Box>
+              <IconButton aria-label="discord">
+                <FacebookIcon />
+              </IconButton>
+              <IconButton aria-label="facebook">
+                <discordIcon/>
+              </IconButton>
+            </Box>
           </Box>
         </Box>
         <p>Made by Maciek for Liga ZMG</p>
