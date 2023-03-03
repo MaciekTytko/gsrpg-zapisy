a7c64c69d7ef65db36977a34efcee6c5f93ddd1f
MaciekTytko <106463945+MaciekTytko@users.noreply.github.com>
2023-03-03 14:07:35 +0100

    Footer prep

--- src/Containers/Footer.js
+++ src/Containers/Footer.js
@@ -1,9 +1,23 @@
+import { Box, Container } from "@mui/material";
 import "./Footer.scss"
 
-function Footer(){
-  return(
+function Footer() {
+  return (
     <div className="Foot d-print-none">
-      <p>Made by Maciek for Liga ZMG</p>
+      <Container>
+        <Box sx={{ pt:'30px', display: 'flex', justifyContent: 'space-around' }}>
+          <Box>
+            <p>O serwisie</p>
+            <p>Polityka prywatności</p>
+          </Box>
+          <Box>
+            <p>Liga ZMG</p>
+            <p>Kontakt</p>
+            <p>sociale</p>
+          </Box>
+        </Box>
+        <p>Made by Maciek for Liga ZMG</p>
+      </Container>
     </div>
   )
 }
